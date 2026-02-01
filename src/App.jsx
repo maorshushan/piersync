a5d010e0fcbb013e058582cf72637192f36e75a4
Maor Shushan <mshushan45@gmail.com>
2026-02-01 13:53:46 +0200

    routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import WishlistPage from './pages/WishlistPage'
 import AppHeader from './components/AppHeader'
 import BottomNav from './components/BottomNav'
 
+const BASE_PATH = '/piersync';
+
 function App() {
   return (
     <div className="flex min-h-screen flex-col bg-bg">
       <AppHeader />
       <main className="flex-1 min-h-0 pt-14 pb-20">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/designer" element={<Designer />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path={ BASE_PATH + "/"} element={<Home />} />
+          <Route path={ BASE_PATH + "/designer"} element={<Designer />} />
+          <Route path={BASE_PATH + "/wishlist"} element={<WishlistPage />} />
         </Routes>
       </main>
       <BottomNav />
